bbaf7643a0aa5dd5844a1a8c180bcc94f5a4ee96
WilliamLewww <WilliamLew32@gmail.com>
2015-11-04 17:30:46 -0800

    initial

---
+++ Memory Seeker/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace Memory_Seeker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "25";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(119, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(69, 21);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 38);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "Memory Tester";
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1});
+            this.listView1.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.None;
+            this.listView1.Location = new System.Drawing.Point(12, 64);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(176, 135);
+            this.listView1.TabIndex = 4;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Address";
+            this.columnHeader1.Width = 100;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(119, 39);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(69, 19);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 205);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 6;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(12, 230);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 7;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(119, 230);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(69, 20);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Modify";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(119, 205);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(69, 20);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Select";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(200, 263);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Whoops :)";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Memory Seeker/Form1.cs
@@ -0,0 +1,278 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace Memory_Seeker
+{
+    public partial class Form1 : Form
+    {
+        Kernel kernel = new Kernel();
+        Dictionary<string, int> memoryList = new Dictionary<string, int>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (memoryList.Count > 0)
+            {
+                listView1.Items.Clear();
+                memoryList = kernel.SearchMemoryByInt(textBox2.Text, int.Parse(textBox1.Text), 2147418111, memoryList);
+                foreach (KeyValuePair<string, int> memory in memoryList)
+                    listView1.Items.Add(memory.Key + ": " + memory.Value);
+            }
+            else
+            {
+                memoryList = kernel.SearchMemoryByInt(textBox2.Text, int.Parse(textBox1.Text), 2147418111);
+                foreach (KeyValuePair<string, int> memory in memoryList)
+                    listView1.Items.Add(memory.Key + ": " + memory.Value);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            memoryList.Clear();
+            listView1.Items.Clear();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (textBox3.Text.Substring(0, 2) == "0x")
+                kernel.WriteMemory(textBox2.Text, int.Parse(textBox3.Text.Substring(2), System.Globalization.NumberStyles.HexNumber), int.Parse(textBox4.Text));
+            else
+                kernel.WriteMemory(textBox2.Text, int.Parse(textBox3.Text, System.Globalization.NumberStyles.HexNumber), int.Parse(textBox4.Text));
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                bool addCharacter = true;
+                string address = "";
+
+                for (int x = 0; x < listView1.SelectedItems[0].Text.Length; x++)
+                {
+                    if (addCharacter == true)
+                    {
+                        if (listView1.SelectedItems[0].Text[x] != ':')
+                            address += listView1.SelectedItems[0].Text[x];
+                        else
+                            addCharacter = false;
+                    }
+                }
+
+                textBox3.Text = address;
+            }
+        }
+    }
+
+    class Kernel
+    {
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr OpenProcess(int dwDesiredAccess, bool bInheritHandle, int dwProcessId);
+
+        [DllImport("kernel32.dll")]
+        public static extern bool ReadProcessMemory(int hProcess, int lpBaseAddress, byte[] lpBuffer, int dwSize, ref int lpNumberOfBytesRead);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, uint nSize, out int lpNumberOfBytesWritten);
+
+        [DllImport("kernel32.dll")]
+        static extern void GetSystemInfo(out SYSTEM_INFO lpSystemInfo);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern int VirtualQueryEx(IntPtr hProcess, IntPtr lpAddress, out MEMORY_BASIC_INFORMATION lpBuffer, uint dwLength);
+
+        [DllImport("kernel32.dll")]
+        public static extern Int32 CloseHandle(IntPtr hProcess);
+
+        const int PROCESS_QUERY_INFORMATION = 0x0400;
+        const int MEM_COMMIT = 0x00001000;
+        const int PAGE_READWRITE = 0x04;
+        const int PROCESS_VM_READ = 0x0010;
+        const int PROCESS_ALL_ACCESS = 0x001F0FFF;
+
+        public struct MEMORY_BASIC_INFORMATION
+        {
+            public int BaseAddress;
+            public int AllocationBase;
+            public int AllocationProtect;
+            public int RegionSize;
+            public int State;
+            public int Protect;
+            public int lType;
+        }
+
+        public struct SYSTEM_INFO
+        {
+            public ushort processorArchitecture;
+            ushort reserved;
+            public uint pageSize;
+            public IntPtr minimumApplicationAddress;
+            public IntPtr maximumApplicationAddress;
+            public IntPtr activeProcessorMask;
+            public uint numberOfProcessors;
+            public uint processorType;
+            public uint allocationGranularity;
+            public ushort processorLevel;
+            public ushort processorRevision;
+        }
+
+        public void WriteMemory(string processName, int address, int value)
+        {
+            var process = OpenProcess(PROCESS_ALL_ACCESS, false, Process.GetProcessesByName(processName)[0].Id);
+            var byteValue = new byte[] { (byte)value };
+
+            int written = 0;
+            WriteProcessMemory(process, new IntPtr(address), byteValue, (UInt32)byteValue.LongLength, out written);
+
+            CloseHandle(process);
+        }
+
+        public Dictionary<string, int> SearchMemoryByInt(string processName, int value, int maxAddress)
+        {
+            Dictionary<string, int> memoryList = new Dictionary<string, int>();
+
+            SYSTEM_INFO sys_info = new SYSTEM_INFO();
+            GetSystemInfo(out sys_info);
+
+            IntPtr proc_min_address = sys_info.minimumApplicationAddress;
+            IntPtr proc_max_address = (IntPtr)(maxAddress);
+
+            long proc_min_address_l = (long)proc_min_address;
+            long proc_max_address_l = (long)proc_max_address;
+
+            Process process;
+            if (processName.EndsWith(".exe")) process = Process.GetProcessesByName(processName.Substring(0, processName.Length - 4))[0];
+            else process = Process.GetProcessesByName(processName)[0];
+
+            IntPtr processHandle = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, false, process.Id);
+
+            MEMORY_BASIC_INFORMATION mem_basic_info = new MEMORY_BASIC_INFORMATION();
+
+            int bytesRead = 0;
+
+            while (proc_min_address_l < proc_max_address_l)
+            {
+                VirtualQueryEx(processHandle, proc_min_address, out mem_basic_info, 28);
+
+                if (mem_basic_info.Protect == PAGE_READWRITE && mem_basic_info.State == MEM_COMMIT)
+                {
+                    byte[] buffer = new byte[mem_basic_info.RegionSize];
+
+                    ReadProcessMemory((int)processHandle, mem_basic_info.BaseAddress, buffer, mem_basic_info.RegionSize, ref bytesRead);
+
+                    for (int i = 0; i < mem_basic_info.RegionSize; i++)
+                    {
+                        if ((buffer[i]) == value)
+                            memoryList.Add("0x" + (mem_basic_info.BaseAddress + i).ToString("X"), buffer[i]);
+                    }
+                }
+
+                proc_min_address_l += mem_basic_info.RegionSize;
+                proc_min_address = new IntPtr(proc_min_address_l);
+            }
+
+            return memoryList;
+        }
+
+        public Dictionary<string, int> SearchMemoryByInt(string processName, int value, int maxAddress, Dictionary<string, int> memoryList)
+        {
+            Dictionary<string, int> newMemoryList = new Dictionary<string, int>();
+
+            SYSTEM_INFO sys_info = new SYSTEM_INFO();
+            GetSystemInfo(out sys_info);
+
+            IntPtr proc_min_address = sys_info.minimumApplicationAddress;
+            IntPtr proc_max_address = (IntPtr)(maxAddress);
+
+            long proc_min_address_l = (long)proc_min_address;
+            long proc_max_address_l = (long)proc_max_address;
+
+            Process process;
+            if (processName.EndsWith(".exe")) process = Process.GetProcessesByName(processName.Substring(0, processName.Length - 4))[0];
+            else process = Process.GetProcessesByName(processName)[0];
+
+            IntPtr processHandle = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, false, process.Id);
+
+            MEMORY_BASIC_INFORMATION mem_basic_info = new MEMORY_BASIC_INFORMATION();
+
+            int bytesRead = 0;
+
+            while (proc_min_address_l < proc_max_address_l)
+            {
+                VirtualQueryEx(processHandle, proc_min_address, out mem_basic_info, 28);
+
+                if (mem_basic_info.Protect == PAGE_READWRITE && mem_basic_info.State == MEM_COMMIT)
+                {
+                    byte[] buffer = new byte[mem_basic_info.RegionSize];
+
+                    ReadProcessMemory((int)processHandle, mem_basic_info.BaseAddress, buffer, mem_basic_info.RegionSize, ref bytesRead);
+
+                    for (int i = 0; i < mem_basic_info.RegionSize; i++)
+                    {
+                        if ((buffer[i]) == value)
+                            if (memoryList.ContainsKey("0x" + (mem_basic_info.BaseAddress + i).ToString("X")))
+                                newMemoryList.Add("0x" + (mem_basic_info.BaseAddress + i).ToString("X"), buffer[i]);
+                    }
+                }
+
+                proc_min_address_l += mem_basic_info.RegionSize;
+                proc_min_address = new IntPtr(proc_min_address_l);
+            }
+
+            return newMemoryList;
+        }
+
+        public Dictionary<string, int> SearchAllMemory(string processName, int maxAddress)
+        {
+            Dictionary<string, int> memoryList = new Dictionary<string, int>();
+
+            SYSTEM_INFO sys_info = new SYSTEM_INFO();
+            GetSystemInfo(out sys_info);
+
+            IntPtr proc_min_address = sys_info.minimumApplicationAddress;
+            IntPtr proc_max_address = (IntPtr)(maxAddress);
+
+            long proc_min_address_l = (long)proc_min_address;
+            long proc_max_address_l = (long)proc_max_address;
+
+            Process process;
+            if (processName.EndsWith(".exe")) process = Process.GetProcessesByName(processName.Substring(0, processName.Length - 4))[0];
+            else process = Process.GetProcessesByName(processName)[0];
+
+            IntPtr processHandle = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, false, process.Id);
+
+            MEMORY_BASIC_INFORMATION mem_basic_info = new MEMORY_BASIC_INFORMATION();
+
+            int bytesRead = 0;
+
+            while (proc_min_address_l < proc_max_address_l)
+            {
+                VirtualQueryEx(processHandle, proc_min_address, out mem_basic_info, 28);
+
+                if (mem_basic_info.Protect == PAGE_READWRITE && mem_basic_info.State == MEM_COMMIT)
+                {
+                    byte[] buffer = new byte[mem_basic_info.RegionSize];
+
+                    ReadProcessMemory((int)processHandle, mem_basic_info.BaseAddress, buffer, mem_basic_info.RegionSize, ref bytesRead);
+
+                    for (int i = 0; i < mem_basic_info.RegionSize; i++)
+                    {
+                        if ((buffer[i]) != 0)
+                            memoryList.Add("0x" + (mem_basic_info.BaseAddress + i).ToString("X"), buffer[i]);
+                    }
+                }
+
+                proc_min_address_l += mem_basic_info.RegionSize;
+                proc_min_address = new IntPtr(proc_min_address_l);
+            }
+
+            return memoryList;
+        }
+    }
+}
